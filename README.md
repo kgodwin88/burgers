55b3e5bec05a37ae302f65b07be33a7c79f3b3ce
kgodwin88 <kgodwin1988@aol.com>
2018-06-24 13:47:03 -0400

    updated read me

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# burgers
-Burgers
+# Burgers
+
+* Allows the user to create a new burger that will go into the section of burgers
+    that need to be devoured 
+* After creating the burger there will be a button that will allow the user to devour the burger
+* After the user devours the burger the burger will move to the section of devoured burgers
+
--- config/connection.js
+++ config/connection.js
@@ -1,20 +1,24 @@
 // Set up MySQL connection.
 var mysql = require("mysql");
 require("dotenv").config();
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: process.env.password,
-  database: "burgers_db"
-});
-
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
+var connection;
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: process.env.password,
+        database: "burgers_db"
+    });
+};
+connection.connect(function (err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
 });
 
 
